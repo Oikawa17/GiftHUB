7b686c60d430bb8299622f6ca69e7cc4fb9429f3
kurt <kbclark@paterostechnologocalcollege.edu.ph>
2025-02-20 17:52:39 +0800

    Final Copy of prog1

--- prog1.java
+++ prog1.java
@@ -1,5 +1,8 @@
 public class prog1 {
 	public static void main (String[] args){
 	 System.out.prinln("Pateros Technological College");
+	Sytem.out.println("Kurt Smith Clark");
+	Sytem.out.println("BSIT 2-L");
+	Sytem.out.println("Navy Blue");
   }
 }
